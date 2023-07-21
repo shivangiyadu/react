3d5e91cc86cbadd7a475e92810e84970b6724b80
shivangi <shivangi>
2023-07-22 00:31:18 +0530

    hello

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Video from "./components/Video";
+
+function App()
+
+{
+  return (
+    <div>
+  <div>hello </div>
+  <Video></Video>
+  </div>
+  )
+}
+export default App; 
---
+++ src/components/Video.js
@@ -0,0 +1,13 @@
+function Video()
+{
+    return (
+    <div>
+        <img src="https://dummyimage.com/160x90/000/fff"
+         alt="dummy images">
+        </img>
+          <div> React Js tutorial-components  </div>
+
+    </div>
+    )
+}
+export default Video;
